be613d191188ea577975d859ca71816b54a43ddf
shotbit <132007651+shotbit@users.noreply.github.com>
2025-07-07 01:22:33 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Idle Animation'
+
+client_scripts {
+    'client.lua'
+}
+
+files {
+    'stream/move_m@generic_idles@std.ycd'
+}
+
+data_file 'ANIM_DICT' 'stream/move_m@generic_idles@std.ycd'
